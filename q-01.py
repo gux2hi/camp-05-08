2f67cd7d2e4a15f559229fa522774a81601a5301
gux2hi <114000887+gux2hi@users.noreply.github.com>
2024-05-08 18:36:27 +0900

    Update q-01.py

--- q-01.py
+++ q-01.py
@@ -1,17 +1,1 @@
-A = int(input())
 
-x, y = 0, 0
-direction = 0
-steps = 0
-
-for i in range(A):
-    step = steps[i]
-    if direction == 0:
-        y += step
-    elif direction == 1:
-        x -= step
-    elif direction == 2:
-        y -= step
-    elif direction == 3:
-        x += step
-print(x,y)
