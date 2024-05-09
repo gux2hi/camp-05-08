b766feb8938b70f46e3c5775f61c4a7fa213cf99
gux2hi <114000887+gux2hi@users.noreply.github.com>
2024-05-09 16:23:37 +0900

    Update q-01.py

--- q-01.py
+++ q-01.py
@@ -1,17 +1,21 @@
-A = int(input())
+N = int(input())
+a = [int(input()) for _ in range(N)]
 
-x, y = 0, 0
-direction = 0
-steps = 0
 
-for i in range(A):
-    step = steps[i]
-    if direction == 0:
-        y += step
-    elif direction == 1:
-        x -= step
-    elif direction == 2:
-        y -= step
-    elif direction == 3:
-        x += step
-print(x,y)
+now_direction = 0  
+x = 0 
+y = 0 
+
+for i in range(N):
+    match now_direction:
+        case 0:
+            y += a[i]
+        case 1:
+            x += a[i]
+        case 2:
+            y -= a[i]
+        case 3:
+            x -= a[i]
+    now_direction = (now_direction + 1) % 4
+
+print(f'{x} {y}')
